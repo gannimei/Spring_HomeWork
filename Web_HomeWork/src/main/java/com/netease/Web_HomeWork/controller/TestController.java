6c64ded70d11019c3baaac0d0178ad8d067e9208
gannimei <tczhoulan@163.com>
2017-12-24 22:56:05 +0800

    complete

--- Web_HomeWork/src/main/java/com/netease/Web_HomeWork/controller/TestController.java
+++ Web_HomeWork/src/main/java/com/netease/Web_HomeWork/controller/TestController.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import com.netease.Web_HomeWork.bean.User;
 import com.netease.Web_HomeWork.dao.UserDao;
@@ -16,13 +18,22 @@ public class TestController {
 
 	@Autowired
 	private UserDao userDao;
-	
+
 	@RequestMapping(value = "/userlist")
-	public ModelAndView UserList() {
-		ModelAndView result = new ModelAndView("userlist");
-		List<User> userList = userDao.GetUserList();
-		result.addObject("userList", userList);
-		return result;
+	public ModelAndView UserList(@RequestHeader("Accept") String accept) {
+		System.out.println(accept);
+		if (accept.equals("text/html")) {
+			ModelAndView result = new ModelAndView("userlist");
+			List<User> userList = userDao.GetUserList();
+			result.addObject("userList", userList);
+			return result;
+		}
+		else {
+			ModelAndView result = new ModelAndView(new MappingJackson2JsonView());
+			List<User> userList = userDao.GetUserList();
+			result.addObject("userList", userList);
+			return result;
+		}
 	}
-	
+
 }
